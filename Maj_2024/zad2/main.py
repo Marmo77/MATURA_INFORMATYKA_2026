7d2eadf8685a372d7cd5d498fd75fb06fc650a7f
Marmo77 <matyskamela@gmail.com>
2026-02-04 15:10:20 +0100

    2024 maj inf - 2.2

--- Maj_2024/zad2/main.py
+++ Maj_2024/zad2/main.py
@@ -11,8 +11,12 @@
 # liczby parzyste zostają zmniejszone o polowe np. 8 => 4
 # liczby nieparzyste zostaja zamienione na 1
 # c = c + b wykona się tyle razy ile jest nieparzystych liczb w algorytmie
+# jesli 0 to pozostaje 0 ale nie robi (! petli c = c+b !)
 
 # c = c + b * 0
+
+
+# 2.1 
 #odp.
 #1)
 
@@ -29,3 +33,10 @@
 # c = 41312111011121314
 # powtorzenia:
 # 8
+
+
+#2.2.   
+
+# 999999999666666333
+# wartosc c:
+# c = 111111111333333111
