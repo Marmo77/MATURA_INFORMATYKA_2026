01ac32a3ed3163196533a963c7c773683a48b574
Marmo77 <matyskamela@gmail.com>
2026-02-04 15:03:00 +0100

    zadanie 2 maj 2024 inf

---
+++ Maj_2024/zad2/main.py
@@ -0,0 +1,31 @@
+# jesli a jest parzyste to zwroc c + polowa a = c = wynik zwracany
+
+# jesli liczba jest nieparzysta to c = dla danego miesjsca = 1 np. 336 => 113
+
+# c = c + b wykonuje sie tyle razy co nieparzyste liczby.
+# 33658  -> 11314 -> 3 razy
+
+
+# wynik z n jest zwracany na takich warunkach:
+
+# liczby parzyste zostają zmniejszone o polowe np. 8 => 4
+# liczby nieparzyste zostaja zamienione na 1
+# c = c + b wykona się tyle razy ile jest nieparzystych liczb w algorytmie
+
+# c = c + b * 0
+#odp.
+#1)
+
+# n = 542102
+# wartosc c:
+# c = 121101 
+# powtorzenia:
+# 2
+
+#2)
+
+# n = 87654321012345678
+# wartosc c:
+# c = 41312111011121314
+# powtorzenia:
+# 8
