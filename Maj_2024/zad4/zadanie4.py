64ce48b6866462f207814eec9d61e6afcef7dca0
Marmo77 <matyskamela@gmail.com>
2026-02-05 13:48:02 +0100

    zadanie 4.2.

--- Maj_2024/zad4/zadanie4.py
+++ Maj_2024/zad4/zadanie4.py
@@ -1,21 +1,18 @@
 
 
-plik = open("liczby_przyklad.txt", "r")
+plik = open("liczby.txt", "r")
+linie = plik.readlines()
+
+linia1: list[int] = linie[0].strip().split(" ") # dzielniki
+linia2: list[int] = linie[1].strip().split(" ") # liczby
 
 
 #zadanie 4.1.
-linie = plik.readlines()
 
-dzielniki: list[int] = linie[0].strip().split(" ")
-print(dzielniki)
-liczby: list[int] = linie[1].strip().split(" ")
-# liczby = liczby.split(" ")
-# for dziel in dzielniki:
-#     print(int(dziel))
-# print(liczby)
+
 ile_razy_dzielnik = 0
-for dzielnik in dzielniki:
-    for liczba in liczby:
+for dzielnik in linia1:
+    for liczba in linia2:
         # dla każdego dzielnika sprawdzamy czy jest podzielny przez liczbę
         if int(liczba) % int(dzielnik) == 0:
             # print("jest dzielnikiem")
@@ -24,3 +21,22 @@ for dzielnik in dzielniki:
 
 print(f"Dzielone są: {ile_razy_dzielnik}")
             
+            
+            
+#zadanie 4.2.
+posortowana = linia1
+# posortowana.sort() # mozna tak posortowac
+
+
+#albo tak posortowac np
+for i in range(len(posortowana)):
+    for j in range(0, len(posortowana) - 1 - i):
+        if int(posortowana[j]) < int(posortowana[j+1]): # jeśli pierwsza jest wieksza od tej stylu to zamien miejscami
+        # w zadaniu jest od najwiekszej do najmniejszej wiec znak w porownaniu '<' pierwsza jest mniejsza od drugiej to podmien
+            posortowana[j], posortowana[j+1] = posortowana[j+1], posortowana[j] #podmianka
+            
+# print(posortowana)
+
+liczba_w_kolejnosci = posortowana[100] # 101 liczbą czyli index 100 bo jest liczony od 0 czyli 101 element od 0 do 100 to 101 elementow
+
+print(liczba_w_kolejnosci)
