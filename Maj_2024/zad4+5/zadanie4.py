31adfb105f1ff8fc93b42bd3261f0d39f8c1c9f4
Marmo77 <matyskamela@gmail.com>
2026-02-05 14:20:30 +0100

    zadanie 4.3.

--- Maj_2024/zad4+5/zadanie4.py
+++ Maj_2024/zad4+5/zadanie4.py
@@ -1,6 +1,6 @@
 
 
-plik = open("liczby.txt", "r")
+plik = open("liczby_przyklad.txt", "r")
 linie = plik.readlines()
 
 linia1: list[int] = linie[0].strip().split(" ") # dzielniki
@@ -40,3 +40,42 @@ for i in range(len(posortowana)):
 liczba_w_kolejnosci = posortowana[100] # 101 liczbą czyli index 100 bo jest liczony od 0 czyli 101 element od 0 do 100 to 101 elementow
 
 print(liczba_w_kolejnosci)
+
+print("-----------")
+#zadanie 4.3.
+
+mozna_obliczyc: list[int] = []
+
+for i in range(len(linia2)): #dla kazdej liczby calkowietej
+    liczba_przed_zmianami = linia2[i] # trzeba takie dodać bo inaczej ta liczba zostanie zmodyfikowana na 1 i wynik będzie 1
+    
+    for j in range(len(linia1)): # przechodzimy przez wszystkie dzielniki
+        if int(linia2[i]) % int(linia1[j]) == 0: # jesli nasza liczba calkowita po dzieleniu przez dzielnik da 0 czyli się przez nią dzieli
+            linia2[i] = int(linia2[i]) / int(linia1[j]) # to zmniejszamy naszą liczbę o wartość z dzielenia
+    # po zakończeniu dzielenia l.całkowitej jeśli wynik z dzielenia 1 czyli jest w stanie się przez nie dzielić to dodajemy do tablicy z liczbami (uzywamy liczby pomocniczej przed zmianami, poniewaz jesli teraz dodamy zwykla to bedize wynosila 1)
+    if linia2[i] == 1:
+        mozna_obliczyc.append(int(liczba_przed_zmianami))
+        
+        
+# print(mozna_obliczyc)
+print(", ".join(str(liczba) for liczba in mozna_obliczyc))
+
+#zadanie 4.4.
+
+
+linia1: list[int] = linie[0].strip().split(" ") # dzielniki
+linia2: list[int] = linie[1].strip().split(" ") # liczby
+
+print(linia2)
+
+
+
+
+
+
+
+# zad 5
+
+# 1. F
+# 2. F
+# 3, P
