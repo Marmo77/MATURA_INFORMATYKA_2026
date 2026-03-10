59e7ccd2ea88483bf7d24f4792c423a9024c9f6b
Marmo77 <matyskamela@gmail.com>
2026-03-10 23:25:19 +0100

    zadanie 3.3 i 3.4.

--- Czerwiec_2025/zadanie3/zadania_3.py
+++ Czerwiec_2025/zadanie3/zadania_3.py
@@ -61,3 +61,58 @@ print(f"Największa liczba powtórzeń to {najwiecej_powtorzen} dla cyfry {najwi
 # print(najwieksza_cyfra,najwiecej_powtorzen)
 
 # --------------------- ZADANIE 3.3. ---------------------
+
+numery_telefonow: list[str] = []
+
+for numer in liczby:
+    if len(numer) == 9:
+        numery_telefonow.append(numer)
+
+numery_telefonow_na_5 = []
+
+for numer in numery_telefonow:
+    if numer.startswith("5"):
+        numery_telefonow_na_5.append(numer)
+
+# print(numery_telefonow_na_5)
+
+# with open("wyniki3_3.txt", "w") as f:
+#     for numer in numery_telefonow_na_5:
+#         f.write(f"{numer}\n")
+
+
+# --------------------- ZADANIE 3.4. ---------------------
+
+
+zbiory_roznych = {}
+for numer in numery_telefonow:
+    rozne: dict[int,int] = {}
+    for cyfra in str(numer):
+        cyfra = int(cyfra)
+        if cyfra not in rozne:
+            rozne[cyfra] = 1
+        else:
+            rozne[cyfra] += 1
+    # print(rozne)
+    zbiory_roznych[numer] = rozne
+# print(numery_telefonow)
+# print(zbiory_roznych)
+
+# teraz sprawdzic ktore maja najmniej keys
+najkrotsze_zbiory = []
+dlugosc_najkrotszego_zbioru = 9999
+for zbiory_roznych in zbiory_roznych.items():
+
+    numer = zbiory_roznych[0]
+    wartosci = zbiory_roznych[1]
+    dlugosc_wartosci = len(wartosci)
+
+    if dlugosc_wartosci < dlugosc_najkrotszego_zbioru: # jesli aktualna dlugosc wartosci jest mniejsza niz poprzednia najwieksza to ja zastepujemy
+        dlugosc_najkrotszego_zbioru = dlugosc_wartosci
+        najkrotsze_zbiory = [] # nowa wartosc najmnijeszych jest ustanowiona wiec czyscimy najdluzszy ciag
+    if dlugosc_wartosci == dlugosc_najkrotszego_zbioru:
+        najkrotsze_zbiory.append([numer, dlugosc_wartosci]) # dodajemy do akutalnych numerów liczby takiej samej dlugosci wartosci jak najkroszy zbior
+
+
+print(dlugosc_najkrotszego_zbioru)
+print(najkrotsze_zbiory)
