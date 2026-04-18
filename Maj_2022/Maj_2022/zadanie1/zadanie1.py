59e101751003b8f59ecacbc102db4cedb5df1968
Marmo77 <matyskamela@gmail.com>
2026-04-18 20:19:10 +0200

    zadanie 1.2.

--- Maj_2022/Maj_2022/zadanie1/zadanie1.py
+++ Maj_2022/Maj_2022/zadanie1/zadanie1.py
@@ -16,10 +16,22 @@ def ciekawe_nudne(n):
         print("nudna")
 
 #CZY NUDNA?
-ciekawe_nudne(4) # NIE
-ciekawe_nudne(229) # NIE
-ciekawe_nudne(82)   # TAK
+# ciekawe_nudne(4) # NIE
+# ciekawe_nudne(229) # NIE
+# ciekawe_nudne(82)   # TAK
 
 print("--------------------------")
 
 #1.2.
+
+def SumaKwCyfr(n):
+    n_copy = n
+    suma = 0
+    while n_copy > 0:
+        ostatnia  = n_copy % 10
+        # print(ostatnia)
+        kw_ostatniej = ostatnia * ostatnia
+        suma += kw_ostatniej
+        n_copy = n_copy // 10
+    return suma
+print(SumaKwCyfr(123))
